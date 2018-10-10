14ab7195cff18de829658ffbb4215e9da317e593
Cezar Sîrbu <sirbunicolaecezar@gmail.com>
2018-10-11 00:22:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -95,15 +95,21 @@ function validate(n, array) {
   return validate(n.childrens[0], [...array, ...n.childrens[0].data]);
 }
 
-let node = new Node(['A->B', 'B->C', 'D->E', 'E->!D']);
+function test(array) {
+  let node = new Node(array);
 
-representation(node);
+  representation(node);
 
-let validation = validate(node, node.data);
+  let validation = validate(node, node.data);
 
-var unique = validation.map(e => removeSpecial(e))
-                       .filter(onlyUnique);
+  var unique = validation.map(e => removeSpecial(e))
+                         .filter(onlyUnique);
 
-console.log(JSON.stringify(node, null, 2));
-console.log(JSON.stringify(validation, null, 2));
-console.log(unique.length === validation.length);
+  console.log(JSON.stringify(node, null, 2));
+  console.log(JSON.stringify(validation, null, 2));
+  console.log(unique.length === validation.length);
+}
+
+// test(['A->B', 'A->!B']);
+test(['A->B', 'B->C', 'D->E', 'E->!D']);
+
